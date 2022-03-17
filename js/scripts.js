de5c56d0befa4b5caf4e2e52ff5b17f1b0be1f52
Yohel Urena <y.urena@paralleldevs.com>
2022-03-17 10:40:31 -0600

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 (function () {
   document.addEventListener("mousemove", parallax); // Add event listener
-  const element = document.querySelector("#parallax"); // Select the element div with id parallax
+  const element1 = document.querySelector("#front-parallax"); // Select the element div with id parallax
+  const element2 = document.querySelector("#bg-parallax"); // Select the element div with id bg-parallax
   function parallax(e) {
     let _width = window.innerWidth / 2; // Get half window width
     let _height = window.innerHeight / 2; // Get half window height
     let _mouseX = e.clientX; //horizontal coordinate
     let _mouseY = e.clientY; //vertical coordinate
-    let _depth1 = `${50 - (_mouseX - _width) * 0.1}% ${50 - (_mouseY - _height) * 0.1}%`;
-    let _depth2 = `${50 - (_mouseX - _width) * 0.2}% ${50 - (_mouseY - _height) * 0.2}%`;
-    let _depth3 = `${50 - (_mouseX - _width) * 0.3}% ${50 - (_mouseY - _height) * 0.3}%`;
-    let bg_position = `${_depth3}, ${_depth2}, ${_depth1}`;
-    //console.log(bg_position);
-    element.style.backgroundPosition = bg_position;
+    //tree leaves
+    let _depth1 = `${50 - (_mouseX - _width) * 0.02}% ${50 - (_mouseY - _height) * 0.02}%`;
+    let _depth2 = `${50 - (_mouseX - _width) * 0.01}% ${50 - (_mouseY - _height) * 0.01}%`;
+    //bg coofe cups
+    let _depth3 = `${50 - (_mouseX - _width) * 0.02}% ${50 - (_mouseY - _height) * 0.02}%`;
+    let _depth4 = `${50 - (_mouseX - _width) * 0.01}% ${50 - (_mouseY - _height) * 0.01}%`;
+    
+    let leaves_position = `${_depth1}, ${_depth2}`;
+    let coofe_cups_position = `${_depth3}, ${_depth4}`;
+
+    element1.style.backgroundPosition = leaves_position;
+    element2.style.backgroundPosition = coofe_cups_position;
   }
 })();
